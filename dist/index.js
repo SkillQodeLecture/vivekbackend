9bd17c91a44cac9549fede324a2442b9e22234b6
SkillQodeLecture <ifmdac80@gmail.com>
2025-04-15 11:41:39 +0530

    15-04-2025

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+// console.log('Hello Backend Services!!!!!!');
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+app.get('/', (req, res) => {
+  res.send('Hello Backend Services!!!');
+});
+app.get('/instagram', (req, res) => {
+  res.send('Hello Instagram Services!!!');
+});
+app.get('/home', (req, res) => {
+  res.send('Hello Home Page Services!!!');
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // console.log('Hello Backend Services!!!!!!');
 
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello Backend Services!!!')
