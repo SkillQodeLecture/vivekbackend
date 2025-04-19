fe8c6cc89b68df24c88f7eedeca2b4d77719881a
vivek kachhadiya <ifmdac80@gmail.com>
2025-04-19 15:13:08 +0530

    19-04-2025

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-let http = require('http');
-let fs = require('fs');
-let port = 4000
+const express = require('express')
 
-const requestUrl = (req , res) => {
-   let filename = "";
-   console.log(req.url);
-   
-   switch(req.url){
-    case '/' : filename = './src/index.html';
-    break
-    case '/home':filename = './src/home.html';
-    break
-   }
-   fs.readFile(filename , (error , result) => {
-    if(!error){
-        res.end(result)
+const port = 8080
+
+const app = express()
+
+app.set('view engine' , 'ejs')
+
+app.get('/' , (req , res) => {
+    return res.render('index')
+})
+
+app.get('/home' , (req , res) => {
+    return res.render('home')
+})
+
+
+app.listen(port , (error) => {
+    if(error){
+        console.log('server not start');
+        return false;
     }
-   })
-}
-
-const server = http.createServer(requestUrl)
-
-server.listen(port , (error) => {
-    if(!error){
-        console.log(`nodejs server start on port ${port}`)
-        return false
-    }
-    console.log('nodejs server not start')
+    console.log(`server has been start on port ${port}`);
 })
